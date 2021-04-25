372ec3c44d6314e4cb97b8e5c22333102073038e
biogolf10457 <biogolf10457@gmail.com>
2021-04-25 19:07:58 +0700

    update transition when scroll

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ function toggleMenu() {
         document.getElementsByClassName("dropmenu")[0].style.display = "block"
     }
 }
+
+function showByClass(eclass) {
+    var elms = document.getElementsByClassName(eclass);
+    var h = window.innerHeight;
+    for (let i = 0; i < elms.length; i++) {
+      if (
+        elms[i].getBoundingClientRect().top > 0 &&
+        elms[i].getBoundingClientRect().top < (3 * h) / 4
+      ) {
+        elms[i].style.opacity = "100%";
+        elms[i].style.transform = "translate(0,0)";
+      }
+    }
+    if (elms[0].getBoundingClientRect().top > (3 * h) / 4) {
+      for (let i = 0; i < elms.length; i++) {
+        elms[i].style.opacity = "0%";
+        elms[i].style.transform = "translate(0,5%)";
+      }
+    }
+  }
+
+window.addEventListener("scroll", (event) => {
+    showByClass('scroll')
+});
+
+showByClass('scroll')
